c25e16eaa0a69e3e4a0348c0dae801e8f211bf54
Hilkiah Makemo <hilmak01@gmail.com>
2018-09-30 04:52:56 -0600

    Modified basic calendar vue

--- Http/routes.php
+++ Http/routes.php
@@ -5,8 +5,9 @@ foreach(['my', 'admin'] as $prefix){
     Route::group([
         'prefix'        => $prefix,
         'middleware'    => ['web', 'Admin'],
-        'namespace'     => 'Admin\Calendar\Http\Controllers'],
-        function(){
-            Route::resource('calendar', CalendarController::class);
-        });
+        'namespace'     => 'Admin\Calendar\Http\Controllers'
+    ],
+    function(){
+        Route::resource('calendar', CalendarController::class);
+    });
 }
--- Resources/assets/components/BasicCalendar.vue
+++ Resources/assets/components/BasicCalendar.vue
@@ -8,7 +8,7 @@
             </ul>
         </div>
         <div class="col">
-            <full-calendar :events="events" :config="config" @event-selected="eventSelected"></full-calendar>
+            <full-calendar :events="events" :event-sources="sourcePath" :config="config" @event-selected="eventSelected"></full-calendar>
         </div>
     </div>
 </template>
@@ -16,33 +16,35 @@
 export default {
     name: "basic-calendar",
     props: [
-
+        "source-path"
     ],
     data() {
         return {
             config: {
                 themeSystem : 'bootstrap4',
                 editable: true,
-                droppable: true
+                droppable: true,
+                defaultView: "month",
+                eventSource: this.sourcePath
             },
-        events: [
-            {
-                title  : 'event1',
-                start  : '2018-09-16',
-                color  : 'darkblue',
-                textColor: 'white'
-            },
-            {
-                title  : 'event2',
-                start  : '2018-09-16',
-                end    : '2018-09-21',
-            },
-            {
-                title  : 'event3',
-                start  : '2018-09-09T12:30:00',
-                allDay : false,
-            },
-        ]
+            events: [
+                {
+                    title  : 'event1',
+                    start  : '2018-09-16',
+                    color  : 'darkblue',
+                    textColor: 'white'
+                },
+                {
+                    title  : 'event2',
+                    start  : '2018-09-16',
+                    end    : '2018-09-21',
+                },
+                {
+                    title  : 'event3',
+                    start  : '2018-09-09T12:30:00',
+                    allDay : false,
+                },
+            ]
         }
     },
     methods: {
